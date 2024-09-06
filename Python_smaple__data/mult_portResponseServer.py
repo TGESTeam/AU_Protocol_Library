846969718ca860cab5477cfe34f40e94a0628ae7
JaeYoonJung08 <whitecy01@naver.com>
2024-09-07 03:05:05 +0900

    feat: 포트 8083 단면도 응답값 파싱

--- Python_smaple__data/mult_portResponseServer.py
+++ Python_smaple__data/mult_portResponseServer.py
@@ -1,34 +1,49 @@
 import socket
 import threading
 import numpy as np
+import cv2
 
 print("Load and print the numpy file")
 load_py = np.load('D:/UnrealData/slicing_sample_data500.npy')
 
 time = -1
-kind = 6
+kind = 1
 #PV port 8081
 flattened_array = load_py[kind, :, :, :, time]
 
-#port 8082
-selectXYZ = 1
-temp = np.rot90(flattened_array, 1, axes=(0,2))
-Port8082z = temp.max(axis=0).reshape(-1)
-Port8082y = temp.max(axis=1).reshape(-1)
-Port8082x = temp.max(axis=2).reshape(-1)
+
 #port 8081
 x, y, z = 10, 10, 10
-flattened_array = flattened_array[x:x+10, y:y+10, z:z+5].reshape(-1)
+re_flattened_array = flattened_array[x:x+10, y:y+10, z:z+5].reshape(-1)
 
 
+#port 8083
+selectXYZ = 1
+temp = np.rot90(flattened_array, 1, axes=(0,2))
 
+# Port8082z = temp.max(axis=0).reshape(-1)
+# Port8082y = temp.max(axis=1).reshape(-1)
+# Port8082x = temp.max(axis=2).reshape(-1)
+
+#port 8083 100x100 resize 필요.
+Port8083z = temp.max(axis=0)
+Port8083y = temp.max(axis=1)
+Port8083x = temp.max(axis=2)
+
+resized_Port8083x = cv2.resize(Port8083x, (100, 100), interpolation=cv2.INTER_LINEAR)
+resized_Port8083y = cv2.resize(Port8083y, (100, 100), interpolation=cv2.INTER_LINEAR)
+resized_Port8083z = cv2.resize(Port8083z, (100, 100), interpolation=cv2.INTER_LINEAR)
+
+resized_Port8083x = resized_Port8083x.reshape(-1)
+resized_Port8083y = resized_Port8083y.reshape(-1)
+resized_Port8083z = resized_Port8083z.reshape(-1)
 
 # parshing port 8081
-formatted_string = ",".join(map(str, flattened_array))
-# parshing port 8082
-Port8082z = ",".join(map(str, Port8082z))
-Port8082y = ",".join(map(str, Port8082y))
-Port8082x = ",".join(map(str, Port8082x))
+formatted_string = ",".join(map(str, re_flattened_array))
+# parshing port 8083
+resized_Port8083x = ",".join(map(str, resized_Port8083x))
+resized_Port8083y = ",".join(map(str, resized_Port8083y))
+resized_Port8083z = ",".join(map(str, resized_Port8083z))
 print(formatted_string)
 
 def handle_client(client_socket, server_id, port):
@@ -65,13 +80,13 @@ def handle_client(client_socket, server_id, port):
                             break 
                     # x 일 때
                     if selectedXYZ == 0:
-                        response = f"{Port8082x}"
+                        response = f"{resized_Port8083x}"
                     # y 일 때
                     elif selectedXYZ == 1:
-                        response = f"{Port8082y}"
+                        response = f"{resized_Port8083y}"
                     # z 일 때
                     elif selectedXYZ == 2:
-                        response = f"{Port8082z}"
+                        response = f"{resized_Port8083z}"
                 # response = f"Server {server_id} acknowledges: {message}"
                 client_socket.send(response.encode('utf-8'))
         except ConnectionResetError:
